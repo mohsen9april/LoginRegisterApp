5e868faa7a74c32b65ce0918fc853bfabf8aaa99
Mohsen Abdollahi <mohsenabdollahi@mohsens-macbook-pro.local>
2019-05-24 14:10:29 +0430

    Login and Work Correctly !

--- LoginRegisterApp/Controller/LoginVC.swift
+++ LoginRegisterApp/Controller/LoginVC.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
+    
+    
+    
+    
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        activityIndicator.isHidden = true
     }
+    
+    
+    @IBAction func forgotClicked(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let email = emailTxt.text , !email.isEmpty,
+              let password = passTxt.text , !password.isEmpty else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error)  in
+            
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.isHidden = true
+            print("Login was successful !")
+            
+            
+        }
+    }
+    
+    
+    @IBAction func guestClicked(_ sender: Any) {
+    }
+    
+    
 }
 
